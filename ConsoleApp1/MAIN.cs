62df3953d35696f78836f95c8503a3f22e7cbfac
пупа лупа <kostyan.gorogreev@mail.ru>
2022-12-04 14:13:25 +0300

    1

--- ConsoleApp1/MAIN.cs
+++ ConsoleApp1/MAIN.cs
@@ -9,35 +9,10 @@ namespace Program
         public static void Main(string[] args) 
         {
 
-            Console.WriteLine(booleanisPalindrome("Was it a cat I saw?"));
             
-        }
-        public static bool booleanisPalindrome (String text)
-        {
-            Regex r1 = new Regex ([^a - zA - Z0 - 9];
-
-
+            
         }
         
-
-        public static bool ETALON(String text)
-        {
-            string a = new Regex("[^a-zA-Z0-9]").Replace(text.ToLower(), "");
-            StringBuilder reverce = new StringBuilder();
-            for (int i = a.Length -1; i >= 0; i--) 
-            {
-                reverce.Append(a[i]);
-            }
-            return a.Equals(reverce.ToString());
-        }
-       
-
-            
-
-
-
-
-        }
 
             
       
