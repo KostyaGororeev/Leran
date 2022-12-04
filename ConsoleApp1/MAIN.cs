00b5a79b355916edeac35b8e891398eb07e80359
User <User@DESKTOP-QEHVDMA>
2022-12-04 14:09:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/MAIN.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    class MainProgram
+    {
+        public static void Main(string[] args) 
+        {
+
+            Console.WriteLine(booleanisPalindrome("Was it a cat I saw?"));
+            
+        }
+        public static bool booleanisPalindrome (String text)
+        {
+            Regex r1 = new Regex ([^a - zA - Z0 - 9];
+
+
+        }
+        
+
+        public static bool ETALON(String text)
+        {
+            string a = new Regex("[^a-zA-Z0-9]").Replace(text.ToLower(), "");
+            StringBuilder reverce = new StringBuilder();
+            for (int i = a.Length -1; i >= 0; i--) 
+            {
+                reverce.Append(a[i]);
+            }
+            return a.Equals(reverce.ToString());
+        }
+       
+
+            
+
+
+
+
+        }
+
+            
+      
+
+
+
+
+
+
+     
+
+
+
+    }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Program
+{
+    public class MainClass
+    {
+        public MainClass()
+        {
+
+        }
+
+        public void sayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
