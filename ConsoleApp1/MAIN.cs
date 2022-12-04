28331cded37ffc6cbe638261bfb8ef613ccb5ef9
пупа лупа <kostyan.gorogreev@mail.ru>
2022-12-04 14:12:59 +0300

    gotovo

--- ConsoleApp1/MAIN.cs
+++ ConsoleApp1/MAIN.cs
@@ -6,36 +6,13 @@ namespace Program
 {
     class MainProgram
     {
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
 
-            Console.WriteLine(booleanisPalindrome("Was it a cat I saw?"));
-            
         }
-        public static bool booleanisPalindrome (String text)
+
+        public static void Test()
         {
-            Regex r1 = new Regex ([^a - zA - Z0 - 9];
-
-
-        }
-        
-
-        public static bool ETALON(String text)
-        {
-            string a = new Regex("[^a-zA-Z0-9]").Replace(text.ToLower(), "");
-            StringBuilder reverce = new StringBuilder();
-            for (int i = a.Length -1; i >= 0; i--) 
-            {
-                reverce.Append(a[i]);
-            }
-            return a.Equals(reverce.ToString());
-        }
-       
-
-            
-
-
-
 
         }
 
